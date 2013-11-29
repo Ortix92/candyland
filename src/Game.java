866a23f40ff4388ec2f9d120de28fa1f5828565a
Ortix92 <nicktsutsunava@gmail.com>
2013-11-29 14:13:53 +0100

    game state manager smerig gefixt
    nu wordt frame doorgegeven aan gsm

--- src/Game.java
+++ src/Game.java
@@ -6,15 +6,13 @@ import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 
 public class Game extends JFrame implements KeyListener {
+	public GameStateManager gsm;
 
-	public GameStateManager gsm = new GameStateManager();
-	public MazeRunner mazerunner = new MazeRunner();
-	public Menu menu = new Menu(gsm);
 	public JFrame frame = new JFrame();
 
 	public Game() {
-		frame.add(menu);
-
+		gsm = new GameStateManager();
+		frame.add(gsm.menu);
 		frame.setSize(1024, 768);
 		frame.addKeyListener(this);
 		frame.setFocusable(true);
@@ -31,19 +29,17 @@ public class Game extends JFrame implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		System.out.println(e.getKeyCode());
-		gsm.setGameState(e.getKeyCode());
-		
+		frame = gsm.setGameState(e.getKeyCode(), frame);
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
--- src/GameStateManager.java
+++ src/GameStateManager.java
@@ -1,15 +1,23 @@
+import javax.swing.JFrame;
+
 public class GameStateManager {
 
+	private static final long serialVersionUID = -2385197562776088374L;
 	private int gameState;
 	public final static int MENU_STATE = 0;
 	public final static int MAZE_STATE = 1;
+	public MazeRunner mazerunner = new MazeRunner();
+	public Menu menu = new Menu(this);
 
 	public GameStateManager() {
 		gameState = MENU_STATE;
 	}
 
-	public void setGameState(int state) {
+	public JFrame setGameState(int state, JFrame frame) {
 		this.gameState = state;
+		frame.add(mazerunner.canvas);
+		return frame;
+		
 	}
 
 	public int getGameState() {
