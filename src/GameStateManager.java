537f92006b4a192e230e71f680df201da00bfc30
RogierVrooman <rogiervrooman@live.nl>
2013-11-29 15:28:42 +0100

    test2

--- src/GameStateManager.java
+++ src/GameStateManager.java
@@ -4,18 +4,40 @@ public class GameStateManager {
 
 	private static final long serialVersionUID = -2385197562776088374L;
 	private int gameState;
-	public final static int MENU_STATE = 0;
-	public final static int MAZE_STATE = 1;
+	public final static int MENU_STATE = 0;	//ESCAPE
+	public final static int MAZE_STATE = 1;	//ENTER
 	public MazeRunner mazerunner = new MazeRunner();
 	public Menu menu = new Menu(this);
-
+	
+	
 	public GameStateManager() {
 		gameState = MENU_STATE;
+
 	}
 
-	public JFrame setGameState(int state, JFrame frame) {
-		this.gameState = state;
-		frame.add(mazerunner.canvas);
+	public JFrame setGameState(int keynumber, JFrame frame) {
+		
+			
+		switch(gameState) {
+		
+			case MENU_STATE:
+				if(keynumber == 10) { 
+					this.gameState = MAZE_STATE;
+					frame.add(mazerunner.canvas);
+				}
+				
+			case MAZE_STATE:
+				if(keynumber == 27) { 
+					this.gameState = MENU_STATE;
+					System.out.println(menu);
+					frame.removeAll();
+					frame.add(menu);
+					frame.setVisible(true);
+				}
+		
+		}
+	
+		
 		return frame;
 		
 	}
